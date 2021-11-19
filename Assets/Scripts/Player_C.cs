0a20c2a0c079b54105bdeb325d83ecf8d535043c
xiaobi <xiaobi@marbleio.tech>
2021-11-19 11:16:56 +0800

    实现植物建造吸附

--- Assets/Scripts/Player_C.cs
+++ Assets/Scripts/Player_C.cs
@@ -10,7 +10,8 @@ public class Player_C : MonoBehaviour
     public GameObject Prefab_Crop_Sunflower;
     // 临时持有的空地
     private GameObject crop_Empty;
-
+    // 全部植物
+    private List<GameObject> crops = new List<GameObject>();
     void Start()
     {
         crop_Empty = GameObject.Instantiate<GameObject>(Prefab_Crop_Empty);
@@ -20,14 +21,57 @@ public class Player_C : MonoBehaviour
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
-        if (Physics.Raycast(ray, out hit, int.MaxValue, 1 << LayerMask.NameToLayer("Ground"))) {
+        if (Physics.Raycast(ray, out hit, int.MaxValue, 1 << LayerMask.NameToLayer("Ground")))
+        {
             // 碰撞点地面
-            if (hit.collider != null && hit.collider.gameObject.tag == "Ground") {
-                // 让鼠标处有一块空地跟着跑
-                crop_Empty.transform.position = hit.point;
+            if (hit.collider != null && hit.collider.gameObject.tag == "Ground")
+            {
+                // 可以吸附的植物
+                GameObject crop = null;
+                // 查找 有没有很近的植物
+                for (int i = 0; i < crops.Count; i++)
+                {
+                    // 如果小于12米
+                    if (Vector3.Distance(hit.point, crops[i].transform.position) < 12)
+                    {
+                        crop = crops[i];
+                        break;
+                    }
+                }
+                if (crop != null)
+                {
+                    // 吸附
+                    Vector3 top = crop.transform.position + new Vector3(0, 0, 10);
+                    Vector3 bottom = crop.transform.position + new Vector3(0, 0, -10);
+                    Vector3 left = crop.transform.position + new Vector3(-10, 0, 0);
+                    Vector3 right = crop.transform.position + new Vector3(10, 0, 0);
+                    Vector3[] points = new Vector3[] { top, bottom, left, right };
+
+                    // 吸附的位置
+                    Vector3 temp = Vector3.zero;
+                    float dis = 10000;
+                    for (int i = 0; i < points.Length; i++)
+                    {
+                        if (Vector3.Distance(hit.point, points[i]) < dis)
+                        {
+                            dis = Vector3.Distance(hit.point, points[i]);
+                            temp = points[i];
+                        }
+                    }
+                    crop_Empty.transform.position = temp;
+                }
+                else
+                {
+                    // 让鼠标处有一块空地跟着跑
+                    crop_Empty.transform.position = hit.point;
+                }
+
             }
-            if (Input.GetMouseButtonDown(0)) {
-                GameObject.Instantiate<GameObject>(Prefab_Crop_Sunflower, hit.point, Quaternion.identity, null);
+            //鼠标左键 建造
+            if (Input.GetMouseButtonDown(0))
+            {
+                GameObject temp = GameObject.Instantiate<GameObject>(Prefab_Crop_Sunflower, crop_Empty.transform.position, Quaternion.identity, null);
+                crops.Add(temp);
             }
         }
     }
